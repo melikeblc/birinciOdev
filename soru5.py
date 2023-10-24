a7442cd8f09ee046682232c25a343b0bc06ae85b
Melike Balcı <balci.melike94@gmail.com>
2023-10-25 01:28:26 +0300

    soru5 deki mesaj düzeltildi<

--- soru5.py
+++ soru5.py
@@ -1,13 +1,12 @@
-#Soru5-Kullanıcıdan alınan bir sayının palindrom olup olmadığını bulan bir program yazınız.
+# Soru5-Kullanıcıdan alınan bir sayının palindrom olup olmadığını bulan bir program yazınız.
 
-#Palindrom sayi, hem sağdan-sola hem soldan-sağa okunduğunda aynı olan sayıdır.
+# Palindrom sayi, hem sağdan-sola hem soldan-sağa okunduğunda aynı olan sayıdır.
 
 sayi = input("Bir sayı giriniz : ")
 
 tersSayi = sayi[::-1]
 
 if sayi == tersSayi:
-    print("Girdiğiniz sayı bir palindrom sayidir.")
-else :
+    print("Girdiğiniz sayı bir palindrom sayıdır.")
+else:
     print("Girdiğiniz sayı palindrom sayı değildir.")
-
